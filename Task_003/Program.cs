e326c91aded30fa58e4fd75d62e9e37b565de009
ritesu <ritesu@mail.ru>
2023-02-02 11:01:37 +1100

    Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите новый размер массива");
+int a = int.Parse(Console.ReadLine()?? "0");
+int[] numbers1 = new int [100];
+int[] numbers2 = new int [a];
+
+void FillMass()
+{
+    for (int i = 0; i < numbers1.Length; i++)
+    {
+        int num = new Random().Next(10);
+        numbers1[i] = num;
+    }
+}
+
+void ResizeMass()
+{
+    for (int i = 0; i < numbers2.Length; i++)
+    {
+        numbers2[i] = numbers1[i];
+    }
+}
+FillMass();
+ResizeMass();
